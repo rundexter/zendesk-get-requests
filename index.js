32d05cf7334e4a3250436723af3d066b50cf9ac6
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-11 20:05:21 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+var util = require('./util.js'),
+    pickInputs = {
+        'id': { key: 'id', validate: { req: true }}
+    }, pickOutputs = {
+        id: 'id',
+        requester_id: 'requester_id',
+        assignee_id: 'assignee_id',
+        collaborator_ids: 'collaborator_ids',
+        created_at: 'created_at',
+        subject: 'subject',
+        description: 'description',
+        status: 'status'
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +21,34 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.environment('zendesk_token'),
+            username = dexter.environment('zendesk_username'),
+            subdomain = dexter.environment('zendesk_subdomain');
+
+        if (!token || !username || !subdomain)
+            return this.fail('A [zendesk_token,zendesk_username,zendesk_subdomain] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        var request = require('request').defaults({
+            baseUrl: 'https://' + subdomain + '.zendesk.com/api/v2/'
+        });
+
+        request.get({
+            uri: '/requests/' + inputs.id + '.json',
+            auth: {
+                user: username.concat('/token'),
+                pass: token
+            },
+            json: true
+        }, function (err, response, result) {
+            if (err || (result && result.error))
+                this.fail(err || result);
+            else
+                this.complete(util.pickOutputs(result.request, pickOutputs));
+        }.bind(this));
     }
 };
